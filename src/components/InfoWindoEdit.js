174667fc980e2aaea60d25c7e78b6e1b7a6ab66d
dev stack <devstack024@gmail.com>
2024-12-04 00:49:10 +0530

    new business

--- src/components/InfoWindoEdit.js
+++ src/components/InfoWindoEdit.js
@@ -230,7 +230,7 @@ const InfoWindowContentEdit = ({ selectedBusiness }) => {
     };
 
     console.log(business.amenities
-        , business.amenities   )
+        , business.amenities)
 
     return (
         <>
@@ -305,8 +305,19 @@ const InfoWindowContentEdit = ({ selectedBusiness }) => {
 
                                 >
                                     <option value="restaurant">Restaurant</option>
+                                    <option value="cafe">Café</option>
+                                    <option value="bar">Bar & Nightclub</option>
                                     <option value="shop">Shop</option>
+                                    <option value="retail">Retail</option>
                                     <option value="service">Service</option>
+                                    <option value="entertainment">Entertainment</option>
+                                    <option value="fitness">Fitness & Gym</option>
+                                    <option value="beauty">Beauty & Spa</option>
+                                    <option value="healthcare">Healthcare</option>
+                                    <option value="education">Education</option>
+                                    <option value="automotive">Automotive</option>
+                                    <option value="hotel">Hotel & Lodging</option>
+                                    <option value="grocery">Grocery & Market</option>
                                 </Form.Select>
                             </Form.Group>
 
